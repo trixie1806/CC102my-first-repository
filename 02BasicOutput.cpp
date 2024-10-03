f94e8fdfd370dd91632da4fdc4534ac6ea7ca817
Trixie Dela Peña <delapenatrixie11@gmail.com>
2024-10-03 10:00:30 +0800

    02BasicOutput.cpp

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "OUR LADY OF LOURDES COLLEGE FOUNDATION";
+	cout<< "COLLEGE OF COMPUTER SCIENCE";
+	cout<< "BSIT 1-A";
+	cout<< "TRIXIE DELA PEÑA";
+	cout<< "P-4 BAGASBAS DAET CAMARINES NORTE";
+	
+	return 0;
+}
