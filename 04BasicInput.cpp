ee46b57cba647ad60e10bb0ce3e9c689e60ac9eb
Trixie Dela Peña <delapenatrixie11@gmail.com>
2024-10-09 10:06:41 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name, course, school;
+	
+	cout << "Nickname: ";
+	cin >> name;
+	cout << "Course-Year Level: ";
+	cin >> course;
+	cout << "School: ";
+	cin >> school;
+	
+	return 0;
+}
