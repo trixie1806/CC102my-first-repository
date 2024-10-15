51ba227fe974f0f5fb21b34bd20808d6fe25a014
Trixie Dela Peña <delapenatrixie11@gmail.com>
2024-10-15 10:08:28 +0800

    Add files via upload

---
+++ CC102_FundamentalsofProgramming_CC++1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sugarPriceUSD, ricePricePoound, sardinesPricePound, coffeepriceUSD, milkPriceUSD;
+	int sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	int sugarcostUSD, ricecostPound, sardinescostPound, coffeecostUSD, milkcostUSD;
+	
+	cout << "sugar cost in USD";
+	cin >> sugarQty;
+	cout << "rice cost in Pound";
+	cin >> riceQty;
+	cout << "sardines cost in Pound";
+	cin >> sardinesQty;
+	cout << "coffee cost in USD";
+	cin >> coffeeQty;
+	cout << "milk cost in USD";
+	cin >> milkQty;
+	
+	
+	
+	return 0;
+}	
